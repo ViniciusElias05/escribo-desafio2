4f9df82947bb712decc031e36cc894a30e348c15
Vinicius Elias <vini-mq@hotmail.com>
2023-11-27 01:12:26 -0300

    delete dist

--- dist/db/connection.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-require("dotenv").config();
-var connection = require("knex")({
-  client: "mysql2",
-  connection: process.env.DATABASE_URL,
-  useNullAsDefault: true
-});
-module.exports = connection;
--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var app = require("./app");
-require("dotenv").config();
-var port = process.env.PORT || 8080;
-app.listen(port, function () {
-  console.log("Rodando...");
-});
